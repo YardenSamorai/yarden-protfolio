dd3b29b1e10046f9bf6950e763c8e271faf2acc1
YardenSamorai <Yardensamorai6@gmail.com>
2022-11-28 17:23:42 +0200

    Adding styled components and styled the components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 //Import Pages
 import AboutUs from "./pages/AboutUs";
+//Global Style
+import GlobalStyle from "./components/GlobalStyle";
 
 function App() {
   return (
     <div>
+      <GlobalStyle />
       <AboutUs />
     </div>
   );
---
+++ src/components/FaqSection.js
@@ -0,0 +1,61 @@
+import React from "react";
+
+const FaqSection = () => {
+  return (
+    <div className="faq">
+      <h2>
+        Any Questions <span>FAQ</span>
+      </h2>
+      <div className="qustion">
+        <h4>How do I Start?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Velit
+            voluptatum possimus architecto, facere nobis unde minus. Dignissimos
+            rerum mollitia possimus, aut optio, temporibus nisi accusamus
+            voluptatum suscipit repellendus, ducimus modi.
+          </p>
+        </div>
+      </div>
+      <div className="qustion">
+        <h4>Daily Schedule</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Velit
+            voluptatum possimus architecto, facere nobis unde minus. Dignissimos
+            rerum mollitia possimus, aut optio, temporibus nisi accusamus
+            voluptatum suscipit repellendus, ducimus modi.
+          </p>
+        </div>
+      </div>
+      <div className="qustion">
+        <h4>Diferrent Payment</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Velit
+            voluptatum possimus architecto, facere nobis unde minus. Dignissimos
+            rerum mollitia possimus, aut optio, temporibus nisi accusamus
+            voluptatum suscipit repellendus, ducimus modi.
+          </p>
+        </div>
+      </div>
+      <div className="qustion">
+        <h4>What Products do you offer</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Velit
+            voluptatum possimus architecto, facere nobis unde minus. Dignissimos
+            rerum mollitia possimus, aut optio, temporibus nisi accusamus
+            voluptatum suscipit repellendus, ducimus modi.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FaqSection;
